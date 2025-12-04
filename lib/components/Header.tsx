29e139eb09c1e631a08fcdcbaf5cb0453f683334
Hanneri <hannerikruger@gmail.com>
2025-12-04 22:15:06 +0200

    about us page

--- lib/components/Header.tsx
+++ lib/components/Header.tsx
@@ -70,7 +70,21 @@ export default function Header() {
               FEEDBACK_LIST
             </button>
           </Link>
+          <Link href="/about">
+            <button className="relative bg-black px-6 py-2 text-sm font-bold tracking-wider text-[#30D6D6] transition-all hover:bg-[#30D6D6] hover:text-black group">
+              {pathname === "/about" && (
+                <>
+                  <span className="absolute top-0 left-0 h-2 w-2 border-t-2 border-l-2 border-[#30D6D6]"></span>
+                  <span className="absolute top-0 right-0 h-2 w-2 border-t-2 border-r-2 border-[#30D6D6]"></span>
+                  <span className="absolute bottom-0 left-0 h-2 w-2 border-b-2 border-l-2 border-[#30D6D6]"></span>
+                  <span className="absolute bottom-0 right-0 h-2 w-2 border-b-2 border-r-2 border-[#30D6D6]"></span>
+                </>
+              )}
+              ABOUT
+            </button>
+          </Link>
         </nav>
+        
       </div>
     </header>
   );
