201598e54815281ca333697fc6bf4436d224136b
Hanneri <hannerikruger@gmail.com>
2025-12-05 19:02:57 +0200

    fixed jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,15 @@
 require("@testing-library/jest-dom");
+const { configure } = require("@testing-library/react");
 
 process.env.NODE_ENV = "test";
 process.env.MONGODB_URI = "mongodb://localhost:27017/test";
 process.env.ANTHROPIC_API_KEY = "test-key-12345";
+
+configure({
+  getElementError: (message) => {
+    const error = new Error(message);
+    error.name = "TestingLibraryElementError";
+    error.stack = "";
+    return error;
+  },
+});
